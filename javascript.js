b733a7caf05468034ee1878b95d0f471389b8198
Jamie Luong <jamieluong03@gmail.com>
2019-09-30 20:41:55 -0700

    updated the html skeleton

---
+++ javascript.js
@@ -0,0 +1,12 @@
+let API = "6ac283a43a431dd845471805e447dc5f";
+
+
+
+// every time user enters a city, create a new li and add to div
+{/* <div class="card" style="width: 18rem;">
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">Cras justo odio</li>
+    <li class="list-group-item">Dapibus ac facilisis in</li>
+    <li class="list-group-item">Vestibulum at eros</li>
+  </ul>
+</div> */}
