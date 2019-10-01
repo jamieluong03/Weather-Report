bd9355c8f571786c0fa6e0573bebb5aa14f05e8d
Jamie Luong <jamieluong03@gmail.com>
2019-10-01 00:43:36 -0700

    updated js file with the temperature, humidity, and windspeed

--- javascript.js
+++ javascript.js
@@ -1,39 +1,66 @@
 let button = $(".btn");
 let input = $("input");
 
-// button.on("click", function(){
-//     // let cityInput = $("input").val();
-//     // let cities = JSON.parse(localStorage.setItem('city', JSON.stringify(cityInput)));
-//     // console.log(cityInput);
+button.on("click", function(){
+    event.preventDefault();
+    let cityInput = $("input").val();
+    console.log(cityInput);
 
-//     // let li = $("<li>");
-//     // let ul = $("ul")
+    // localStorage.setItem('city', JSON.stringify(cityInput));
 
-   
-// })
+    let newDiv = $("<div>");
+    newDiv.attr('class', 'card');
 
-    let cityName = 'London';
+    newDiv.text(cityInput);
+    
+    $("#cityList").prepend(newDiv);
+    
+
     let API = "c04a879c0afe257f716ea92825ba01c6";
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + API;
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput + "&appid=" + API;
     
     $.ajax({
         url: weatherURL,
         method: "GET"
-      })
-        .then(function(response) {
+    }).then(function(response) {
             console.log(response);
 
+            let kelvin = response.main.temp;
+            let humidity = response.main.humidity;
+            let windSpeed = response.wind.speed;
+            let temperature = 1.8 * (kelvin - 273) + 32;
+            let iconValue = response.weather[0].icon;
+            
+
+            $("#cityName").text(cityInput.toUpperCase());
+            $("img").attr('src', 'http://openweathermap.org/img/wn/'+ iconValue +'.png');
+            $(".temp").text("Temperature(F): " + Math.round(10 * temperature )/10);
+            $(".humidity").text("Humidity: " + humidity);
+            $(".wind").text("Wind Speed: " + windSpeed);
+            console.log(iconValue);
+            
+            let lat = response.coord.lat;
+            localStorage.setItem('lat', lat);
+            let lon = response.coord.lon;
+            localStorage.setItem('lon', lon);
+
+            console.log(lat, lon)
         });
+    
+    let latUV = JSON.stringify(localStorage.getItem('lat'));
+    let lonUV = JSON.stringify(localStorage.getItem('lon'));
+    console.log(latUV, lonUV);
+    
+    let UVUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latUV + "&lon=" + lonUV + "&appid=" + API;
+
+    $.ajax({
+        url: UVUrl,
+        method: "GET"
+    }).then(function(response){
+
+    })
+})
+
+    
 
 
-
-
-
-// every time user enters a city, create a new li and add to div
-{/* <div class="card" style="width: 18rem;">
-  <ul class="list-group list-group-flush">
-    <li class="list-group-item">Cras justo odio</li>
-    <li class="list-group-item">Dapibus ac facilisis in</li>
-    <li class="list-group-item">Vestibulum at eros</li>
-  </ul>
-</div> */}
